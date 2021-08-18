be26848c813fc3076fe4815d76987af554539716
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-08-17 21:53:27 -0300

    Atualzando

--- zumbiDice.py
+++ zumbiDice.py
@@ -12,75 +12,106 @@ Onde, o caractere “C” na string corresponde ao cérebro, caractere “P” s
 
 import random
 
-dadoVerde = ["CEREBRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"]
-dadoAmarelo = ["TIRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"]
-dadoVermelho = ["TIRO","PASSO","TIRO","CEREBRO","PASSO","TIRO"]
-StartJogo = False
+dadoVerde = "CEREBRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"
+dadoAmarelo = "TIRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"
+dadoVermelho = "TIRO","PASSO","TIRO","CEREBRO","PASSO","TIRO"
+StartJogo = True
 
 tubo = []
 
 #Adicionar dados no copo
-for i in range(0,6):
-    tubo.append(dadoVerde)
-for i in range(0, 4):
-    tubo.append((dadoAmarelo))
-for i in range(0, 3):
-    tubo.append(dadoVermelho)
+def inserirDados():
+    for i in range(0,6):
+        tubo.append(dadoVerde)
+    for i in range(0, 4):
+        tubo.append((dadoAmarelo))
+    for i in range(0, 3):
+        tubo.append(dadoVermelho)
 
-#Sortear dados:
-DadosUm = random.choice(random.choice(tubo))
-DadosDois = random.choice(random.choice(tubo))
-DadosTres = random.choice(random.choice(tubo))
-
-
-#Jogar 3 dados:
-jogarDados = [DadosUm, DadosDois, DadosTres]
-
-#print(jogarDados)
 
 #Quantos jogadores estão jogando:
 players = int(input("Quantos jogadores vão jogar: "))
 jogadores = []
 
+#Variavel de controle de inicio:
+if StartJogo == True:
+    inserirDados()
+
+cont = 0
+
+
 if players >= 2:
     #Início do jogo:
     StartJogo = True
+    #Adicionando jogadores:
+    i = 1
+    while (i < players + 1):
+        pessoas = dict({'jogador': i, 'cerebros': 0})
+        jogadores.append(pessoas)
+        i += 1
+
+    
 
     while (StartJogo == True):
-        #Adicionando jogadores:
-        i = 1
-        while (i < players + 1):
-            pessoas = dict({'jogador': i, 'cerebros': 0})
-            jogadores.append(pessoas)
-            i += 1
 
         #Escolhas:
-        numJogador = int(input("Qual jogador vai jogar os dados? [1, 2...]: "))
-        jogar = int(input("[1] - Jogar dados ou [2] - Finalizar Turno? "))
-
-        if jogar == 1:
-            
-            valores = jogarDados
-            if valores[0:2] == "TIRO":
-                print('Você foi atingido por 3 tiros e perdeu o turno!')
-            if valores[0] == "CEREBRO":
-                jogadores[numJogador]['cerebros'] += 1
-            if valores[1] == "CEREBRO":
-                jogadores[numJogador]['cerebros'] += 1
-            if valores[2] == "CEREBRO":
-                jogadores[numJogador]['cerebros'] += 1
-
-            #Pontuação do Jogo:
-            
-            print(valores)
-            print(jogadores[numJogador]['cerebros'])
-
-        elif jogar == 2:
-            print("Seu turno finalizou, proximo jogador")
-            StartJogo = False
-        
+        try:
+            numJogador = int(input("Qual jogador vai jogar os dados? [1, 2...]: "))
+            playerAtual = numJogador - 1
+        except:
+            print("Valor inválido!")
         
 
+        turno = False
+        if numJogador != 0:
+            turno = True
+        
+        while (turno == True):
+            #Jogar dados ou sair do turno:
+            try:
+                jogar = int(input("[1] - Jogar dados ou [2] - Finalizar Turno? "))
+            except:
+                print("Valor inválido!")
 
+            if jogar == 1:
+                #Jogando os dados:    
+                dados = []
+                for i in range(0, 3):
+                    facesDados = random.choice(random.choice(tubo))
+                    dados.append(facesDados)
+                    #Adicionando pontos:
+                    try: 
+                        if facesDados == "CEREBRO":
+                            if jogadores[playerAtual]['cerebros'] == 13:
+                                print(f'PARABÉNS o jogador {numJogador} venceu!')
+                                turno = False
+                                StartJogo == False
+                            else:
+                                jogadores[playerAtual]['cerebros'] += 1
+                        del tubo[i]
+                        if len(tubo) == 0:
+                            inserirDados()
+                        elif StartJogo == False:
+                            inserirDados()
+                    except:
+                        print("Preenchendo o tubo")
+                    
+                print(dados)
+
+                #Pontuação do Jogo:
+                print(f'Jogador {numJogador}: ',jogadores[playerAtual]['cerebros'])
+
+            elif jogar == 2:
+                print("Seu turno finalizou, proximo jogador")
+                turno = False
+
+        try:
+            continuar = int(input('continuar o jogo [1] - SIM e [2] - NÃO'))
+            if continuar == 2:
+                StartJogo = False
+        except:
+            print("Valor inválido!")
+
+    
 elif players < 2:
     print("jogadores insuficiente, chame mais amigos para jogar...")
