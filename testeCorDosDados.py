dccd76c84f1b0abf8247da3e51f537af8970347e
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-08-30 19:00:53 -0300

    update

--- testeCorDosDados.py
+++ testeCorDosDados.py
@@ -16,27 +16,80 @@ def inserData():
         tubo.append({"dadoVermelho": dadoVermelho})
 
 mao = []
-faces = []
 
 def sorteio():
     for i in range(0, 3):
         mao.append(random.choice(tubo))
-    for i in range(0, 3):
-        for i in mao:
-            corDado = 
-        
 
 inserData()
 sorteio()
-print(faces)
-print(len(faces))
+
+for i in range(0, 3):
+    if "dadoVermelho" in mao[i]:
+        dadoFace = random.choice(mao[i]["dadoVermelho"])
+        print('Dado Vermelho')
+        print(dadoFace)
+    if "dadoVerde" in mao[i]:
+        dadoFace = random.choice(mao[i]["dadoVerde"])
+        print("Dado Verde.")
+        print(dadoFace)
+    if "dadoAmarelo" in mao[i]:
+        dadoFace = random.choice(mao[i]["dadoAmarelo"])
+        print("Dado Amarelo.")
+        print(dadoFace)
 
 
-#Adicionar dados no copo
-def inserirDados():
-    for i in range(0,6):
-        tubo.append(dadoVerde)
-    for i in range(0, 4):
-        tubo.append((dadoAmarelo))
-    for i in range(0, 3):
-        tubo.append(dadoVermelho)
+
+print(len(mao))
+
+
+#PONTOS DO JOGO NOVO:
+
+try:
+                                if dadoFace == "CEREBRO":
+                                    if jogadores[playerAtual]['cerebros'] == 13:
+                                        print(f'PARABÉNS o jogador {numJogador} venceu!')
+                                        turno = False
+                                        StartJogo == False
+                                    else:
+                                        jogadores[playerAtual]['cerebros'] += 1
+                                elif dadoFace == "TIRO":
+                                    if jogadores[playerAtual]['tiros'] >= 3:
+                                        print(f'Você levou 3 tiros e perdeu o turno.')
+                                        turno = False
+                                        jogadores[playerAtual]['tiros'] = 0
+                                    else:
+                                        jogadores[playerAtual]['tiros'] += 1
+                                del tubo[i]
+                                if len(tubo) == 0:
+                                    inserirDados()
+                                elif StartJogo == False:
+                                    inserirDados()
+                            except:
+                                print("Preenchendo o tubo")
+
+
+#JOGO ANTERIOR:
+#Adicionando pontos:
+                    try: 
+                        if facesDados == "CEREBRO":
+                            if jogadores[playerAtual]['cerebros'] == 13:
+                                print(f'PARABÉNS o jogador {numJogador} venceu!')
+                                turno = False
+                                StartJogo == False
+                            else:
+                                jogadores[playerAtual]['cerebros'] += 1
+                        elif facesDados == "TIRO":
+                            if jogadores[playerAtual]['tiros'] >= 3:
+                                print(f'Você levou 3 tiros e perdeu o turno.')
+                                turno = False
+                                jogadores[playerAtual]['tiros'] = 0
+                            else:
+                                jogadores[playerAtual]['tiros'] += 1
+                        del tubo[i]
+                        if len(tubo) == 0:
+                            inserirDados()
+                        elif StartJogo == False:
+                            inserirDados()
+                    except:
+                        print("Preenchendo o tubo")
--- zumbiDice.py
+++ zumbiDice.py
@@ -1,20 +1,28 @@
 import random
 
-dadoVerde = "CEREBRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"
-dadoAmarelo = "TIRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"
-dadoVermelho = "TIRO","PASSO","TIRO","CEREBRO","PASSO","TIRO"
+dadoVerde = ["CEREBRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"]
+dadoAmarelo = ["TIRO","PASSO","CEREBRO","TIRO","PASSO","CEREBRO"]
+dadoVermelho = ["TIRO","PASSO","TIRO","CEREBRO","PASSO","TIRO"]
 StartJogo = False
 
 tubo = []
 
 #Adicionar dados no copo
 def inserirDados():
-    for i in range(0,6):
-        tubo.append(dadoVerde)
+    for i in range(0, 6):
+        tubo.append({"dadoVerde": dadoVerde})
     for i in range(0, 4):
-        tubo.append((dadoAmarelo))
+        tubo.append({"dadoAmarelo": dadoAmarelo})
     for i in range(0, 3):
-        tubo.append(dadoVermelho)
+        tubo.append({"dadoVermelho": dadoVermelho})
+
+#Rodada do jogo:
+mao = []
+
+def sorteio():
+    mao.append(random.choice(tubo))
+    #for i in range(0, 3):
+    
 
 
 #Quantos jogadores estão jogando:
@@ -64,31 +72,33 @@ if players >= 2:
                 #Jogando os dados:    
                 dados = []
                 for i in range(0, 3):
-                    facesDados = random.choice(random.choice(tubo))
-                    dados.append(facesDados)
-                    #Adicionando pontos:
-                    try: 
-                        if facesDados == "CEREBRO":
-                            if jogadores[playerAtual]['cerebros'] == 13:
-                                print(f'PARABÉNS o jogador {numJogador} venceu!')
-                                turno = False
-                                StartJogo == False
-                            else:
-                                jogadores[playerAtual]['cerebros'] += 1
-                        elif facesDados == "TIRO":
-                            if jogadores[playerAtual]['tiros'] >= 3:
-                                print(f'Você levou 3 tiros e perdeu o turno.')
-                                turno = False
-                                jogadores[playerAtual]['tiros'] = 0
-                            else:
-                                jogadores[playerAtual]['tiros'] += 1
-                        del tubo[i]
-                        if len(tubo) == 0:
-                            inserirDados()
-                        elif StartJogo == False:
-                            inserirDados()
-                    except:
-                        print("Preenchendo o tubo")
+                    #Etapa ANTIGA:
+                    #facesDados = random.choice(random.choice(tubo))
+                    #dados.append(facesDados)
+                    #FIM ETAPA.
+
+                    sorteio()
+
+
+                    #Adicionando Pontos nova etapa:
+                    #Cores e faces dos dados:
+                    for i in range(0, 3):
+                        if "dadoVermelho" in mao[i]:
+                            dadoFace = random.choice(mao[i]["dadoVermelho"])
+                            print('Dado Vermelho')
+                            print(dadoFace)
+                            
+                        if "dadoVerde" in mao[i]:
+                            dadoFace = random.choice(mao[i]["dadoVerde"])
+                            print("Dado Verde.")
+                            print(dadoFace)
+                        if "dadoAmarelo" in mao[i]:
+                            dadoFace = random.choice(mao[i]["dadoAmarelo"])
+                            print("Dado Amarelo.")
+                            print(dadoFace)
+
+                    
+                    
                     
                 print(dados)
 
